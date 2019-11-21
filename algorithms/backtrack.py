de0ef29808d093f436cb39f1f643f54dad4a2081
Jules <julesfarjas@gmail.com>
2019-11-20 23:19:28 -0300

    Getting rid of unneccessary folders

---
+++ algorithms/backtrack.py
@@ -0,0 +1,1 @@
+from helpers import test_graph1, test_graph2, enter_matrix, is_edge, is_in_clique
--- algorithms/helpers.py
+++ algorithms/helpers.py
@@ -1,4 +1,19 @@
-import numpy as np 
+import numpy as np
+
+test_graph1 = np.array([[0, 0, 0, 1, 0, 0],
+                        [0, 0, 0, 0, 1, 0],
+                        [0, 0, 0, 0, 0, 1],
+                        [1, 0, 0, 0, 1, 1],
+                        [0, 1, 0, 1, 0, 1],
+                        [0, 0, 1, 1, 1, 0]])
+
+test_graph2 = np.array([[0, 1, 0, 1, 1, 0],
+                        [1, 0, 1, 0, 1, 0],
+                        [0, 1, 0, 1, 1, 1],
+                        [1, 0, 1, 0, 1, 1],
+                        [1, 1, 1, 1, 0, 1],
+                        [0, 0, 1, 1, 1, 0]])
+
 
 def enter_matrix():
     R = int(input("Enter the number of rows:"))
@@ -12,8 +27,24 @@ def enter_matrix():
     print(repr(matrix))
     return(matrix)
 
+
+def is_edge(u, v, adj_mat):
+    return adj_mat[u-1, v-1]
+
+
+def is_in_clique(v, clique, adj_mat):
+    is_in = True
+    if clique is None:
+        return False
+    for node in clique:
+        if not is_edge(v, node, adj_mat):
+            is_in = False
+    return is_in
+
+
 def main():
     enter_matrix()
+
 
 if __name__ == '__main__':
     main()
