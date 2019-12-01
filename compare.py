edfb249c4b0b0895be8667276798849583ae9828
Jules <julesfarjas@gmail.com>
2019-12-01 14:52:59 -0300

    Adding the new greedy algorithm to compare.py

--- compare.py
+++ compare.py
@@ -4,24 +4,33 @@ import time
 
 from backtrack import backtrack
 from brute_force import brute_force
-from heuristics import greedy2
+from heuristics import greedy, greedy2
 from helpers import load_graph, cliques_from_list
+from test_instances import test_graph1, test_graph2
 
 
 def main():
-    test_graph = load_graph('instance5.clq')
+    test_graph = load_graph('instance3.clq')
 
+    print("\n--- GREEDY ---")
+    start_time = time.time()
+    solution = cliques_from_list(greedy(test_graph))
+    print("--- %s seconds ---" % (time.time() - start_time))
+    print(">>>", len(solution), "cliques")
+
+    print("\n--- BACKTRACK ---")
     start_time = time.time()
     cliques = [0 for x in range(test_graph.shape[0])]
     cliques[0] = 1
     solution = backtrack(test_graph, cliques, 0)
-    print(solution[0], "cliques:", solution[1])
     print("--- %s seconds ---" % (time.time() - start_time))
+    print(">>>", solution[0], "cliques")
 
+    print("\n--- GREEDY2 ---")
     start_time = time.time()
     solution = cliques_from_list(greedy2(test_graph))
-    print(len(solution), "cliques:", solution)
     print("--- %s seconds ---" % (time.time() - start_time))
+    print(">>>", len(solution), "cliques")
 
 
 if __name__ == '__main__':
