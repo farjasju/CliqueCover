d80e4549b963bdd73bce5c837929e2a88c2a8ee1
Jules <julesfarjas@gmail.com>
2019-12-03 15:36:14 -0300

    Improving backtrack

--- backtrack.py
+++ backtrack.py
@@ -6,28 +6,37 @@ from helpers import enter_matrix, is_edge, is_in_clique, is_clique, cliques_from
 from test_instances import test_graph1, test_graph2, test_graph3
 
 
-def backtrack(adj_mat, cliques, v=0,  best=(math.inf, None)):
+def backtrack(adj_mat, cliques, v=1,  best=(math.inf, None)):
     n = adj_mat.shape[0]
+    # print(cliques)
+    # print("CALL: v=" + str(v),
+    #       len(set(list(cliques))-set({0})), "cliques=", cliques)
     if v == n:
         if is_solution(cliques, adj_mat):
-            if len(set(list(cliques))) < best[0]:
+            if len(set(list(cliques))-set({0})) < best[0]:
                 best = (len(set(list(cliques))), cliques_from_list(cliques))
+                # print('new best:', best)
+
     else:
         for i in range(1, v+2):
             cliques[v] = i
             if is_solution(cliques, adj_mat):
-                if len(set(list(cliques))) < best[0]:
+                # print(cliques)
+                if len(set(list(cliques))-set({0})) < best[0]:
+                    # print(len(set(list(cliques))-set({0})), '<', best[0])
                     best = backtrack(adj_mat, cliques, v+1, best)
+                    # print('new best:', best[0], best[1])
+            cliques[v] = 0
     return best
 
 
 def main():
     # test_graph = load_graph('instance3.clq')
-    test_graph = test_graph3
+    test_graph = test_graph1
     start_time = time.time()
     cliques = [0 for x in range(test_graph.shape[0])]
     cliques[0] = 1
-    solution = backtrack(test_graph, cliques, 0)
+    solution = backtrack(test_graph, cliques, 1)
     print(solution[0], "cliques:", solution[1])
     print("--- %s seconds ---" % (time.time() - start_time))
 
