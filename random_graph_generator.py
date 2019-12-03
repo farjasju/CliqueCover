d25329c58815c99549b4e6ed95f14188e9fa3c52
Jules <julesfarjas@gmail.com>
2019-12-03 15:46:15 -0300

    Allowing to generate G(n,p) graphs with n constant

--- random_graph_generator.py
+++ random_graph_generator.py
@@ -5,25 +5,38 @@ import numpy as np
 OUT_DIR = os.path.join('data', 'random_graphs')
 
 
-def generate_graphs(p=0.2, start=10, stop=200, nb=20):
+def generate_graphs(n=20, p=0.2, start=10, stop=200, nb=20, n_constant=False):
     if not os.path.exists(OUT_DIR):
         os.mkdir(OUT_DIR)
     if not os.path.exists(os.path.join(OUT_DIR, 'gexf')):
         os.mkdir(os.path.join(OUT_DIR, 'gexf'))
     if not os.path.exists(os.path.join(OUT_DIR, 'clq')):
         os.mkdir(os.path.join(OUT_DIR, 'clq'))
-    for n in np.linspace(start, stop, nb):
-        n = int(n)
-        g = nx.fast_gnp_random_graph(n, p)
-        filename = 'Gnp' + str(n) + '_' + str(p).strip('.')
-        nx.write_gexf(g, os.path.join(OUT_DIR, 'gexf', filename + '.gexf'))
-        with open(os.path.join(OUT_DIR, 'clq', filename + '.clq'), "w") as f:
-            # writes the header
-            f.write("p EDGE {} {}\n".format(
-                g.number_of_nodes(), g.number_of_edges()))
-            # writes all edges
-            for u, v in g.edges():
-                f.write("e {} {}\n".format(u, v))
+    if not n_constant:
+        for n in np.linspace(start, stop, nb):
+            n = int(n)
+            g = nx.fast_gnp_random_graph(n, p)
+            filename = 'Gnp' + str(n) + '_' + str(p).strip('.')
+            nx.write_gexf(g, os.path.join(OUT_DIR, 'gexf', filename + '.gexf'))
+            with open(os.path.join(OUT_DIR, 'clq', filename + '.clq'), "w") as f:
+                # writes the header
+                f.write("p EDGE {} {}\n".format(
+                    g.number_of_nodes(), g.number_of_edges()))
+                # writes all edges
+                for u, v in g.edges():
+                    f.write("e {} {}\n".format(u, v))
+    else:
+        for p in np.linspace(start, stop, nb):
+            g = nx.fast_gnp_random_graph(n, p)
+            filename = 'Gnp' + str(n) + '_' + str(p).strip('.')
+            nx.write_gexf(g, os.path.join(OUT_DIR, 'gexf', filename + '.gexf'))
+            with open(os.path.join(OUT_DIR, 'clq', filename + '.clq'), "w") as f:
+                # writes the header
+                f.write("p EDGE {} {}\n".format(
+                    g.number_of_nodes(), g.number_of_edges()))
+                # writes all edges
+                for u, v in g.edges():
+                    f.write("e {} {}\n".format(u, v))
 
 
 def main():
