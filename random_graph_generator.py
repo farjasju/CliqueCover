f93f69dd5f285a9ba0c90a0458d16ebe1abe53aa
Jules <julesfarjas@gmail.com>
2019-12-05 19:48:56 -0300

    Adding some comments

--- random_graph_generator.py
+++ random_graph_generator.py
@@ -7,15 +7,21 @@ import shutil
 OUT_DIR = os.path.join('data', 'random_graphs')
 
 
-def generate_graphs(n=20, p=0.2, start=10, stop=200, nb=20, n_constant=False):
+def generate_graphs(n=20, p=0.2, start=10, stop=1000, nb=100, n_constant=False, OUT_DIR=OUT_DIR):
+
+    # cleans the output folders
     shutil.rmtree(os.path.join(OUT_DIR, 'gexf'))
     shutil.rmtree(os.path.join(OUT_DIR, 'clq'))
+
+    # creates the output folders if needed
     if not os.path.exists(OUT_DIR):
         os.mkdir(OUT_DIR)
     if not os.path.exists(os.path.join(OUT_DIR, 'gexf')):
         os.mkdir(os.path.join(OUT_DIR, 'gexf'))
     if not os.path.exists(os.path.join(OUT_DIR, 'clq')):
         os.mkdir(os.path.join(OUT_DIR, 'clq'))
+
+    # n varies, p constant
     if not n_constant:
         for n in np.linspace(start, stop, nb):
             n = int(n)
@@ -29,6 +35,8 @@ def generate_graphs(n=20, p=0.2, start=10, stop=200, nb=20, n_constant=False):
                 # writes all edges
                 for u, v in g.edges():
                     f.write("e {} {}\n".format(u, v))
+
+    # p varies, n constant
     else:
         for p in np.linspace(start, stop, nb):
             g = nx.fast_gnp_random_graph(n, p)
